ec279306b02b3b7a89eca2ec19f9d5aaf84ee51d
Nuria Malet Quintar <maletq.pr@gmail.com>
2022-09-24 14:42:33 -0300

    add details modal

--- client/src/components/Task/index.jsx
+++ client/src/components/Task/index.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import {Container,Icon} from 'semantic-ui-react';
+import {Container,Icon,Modal,Header} from 'semantic-ui-react';
 import AddTask from '../AddTask';
 import './Task.css'
 
@@ -9,6 +9,7 @@ function Task(props) {
     const [isEdited,setIsEdited] = useState(false);
     const [title,setTitle] = useState("");
     const [description,setDescription] = useState("");
+    const [detailsOpen, setDetailsOpen] = useState(false);
 
     const openFromTask = () => {
         setIsEdited(true);
@@ -33,6 +34,15 @@ function Task(props) {
                     <Icon color='yellow' name='edit' onClick={openFromTask}/>
                 </div>
                 <AddTask task={props.task} isEdited={isEdited} IsModalOpened={modalIsOpen} onCloseModal={handleCloseModal} />
+                <Modal closeIcon open={detailsOpen} trigger={<Icon color='blue' name='eye' style={{float:'right'}}/>}
+                        onClose={() => setDetailsOpen(false)} onOpen={() => setDetailsOpen(true)}>
+                    <Header icon='tasks' content='Task Details' style={{backgroundColor:'purple'}}/>
+                    <Modal.Content>
+                        <div><h3>{props.task.title}</h3></div>
+                        <div>{props.task.description}</div>
+                        <div>{props.task.completed ? "tarea completada" : "tarea por completar"}</div>
+                    </Modal.Content>
+                    </Modal>
             </Container>
         </div>
     );
