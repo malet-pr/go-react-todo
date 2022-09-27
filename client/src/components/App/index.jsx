fc988edb88c6a748e573b27915f6eda7063b0194
Nuria Malet Quintar <maletq.pr@gmail.com>
2022-09-26 22:15:19 -0300

    autocomplete works but breaks other functionalities

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -1,24 +1,47 @@
 import React,{useState,useEffect} from 'react';
-import {Header,Icon} from 'semantic-ui-react';
+import {Header,Icon,Button,Form,Popup} from 'semantic-ui-react';
 import TodoList from '../TodoList';
 import Task from '../Task';
 import AddTask from '../AddTask';
 import './App.css';
-import {GetAllTasks,RemoveTask,ToggleTask,ChangeTask} from '../../service/apiCalls.js';
+import {GetAllTasks,RemoveTask,ToggleTask,RemoveAll} from '../../service/apiCalls.js';
 
 
 function App() {
 
   const [tasks, setTasks] = useState([]);
+  const [searchValue,setSearchValue] = useState('');
+  const [newTask,setNewTask] = useState([]);
+  const [title,setTitle] = useState('');
+  const [description,setDescription] = useState('');
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     GetAllTasks().then(setTasks);
   }, []);
 
-  const [newTask,setNewTask] = useState([]);
-  const [title,setTitle] = useState('');
-  const [description,setDescription] = useState('');
-  const [modalIsOpen, setIsOpen] = useState(false);
+  const handleSearchChange = (event) =>{
+    console.log (event.target.value)
+    setSearchValue(event.target.value)
+  }
+
+  let searchTasks = [];
+  if (searchValue <= 0) {
+    searchTasks = tasks;
+  }else {
+    searchTasks = tasks.filter(
+      task => {
+        const taskTitle = task.title.toLowerCase();
+        const searchText = searchValue.toLowerCase();
+        return taskTitle.includes(searchText);
+      }
+    );
+  }
+
+  const deleteAll = (e) => {
+    RemoveAll();
+    setTasks([]);
+  }
 
   function openFromParent() {
     setIsOpen(true);
@@ -30,8 +53,12 @@ function App() {
   }
 
   function addTask(newTask) {
-    const task = newTask;
-    setTasks([...tasks, task]);
+    if(tasks != null){
+      const task = newTask;
+      setTasks([...tasks, task]);
+    } else {
+      setTasks(newTask);
+    }
   }  
 
   function toggleTaskCompleted(_id) {
@@ -66,8 +93,13 @@ function App() {
       <Header className='header' as='h1'>
         Things To Do
       </Header> 
+      <Form>
+        <Form.Field>
+          <input type='text' placeholder="Search Task" value={searchValue} onChange={handleSearchChange}/>
+        </Form.Field>
+      </Form>
       <TodoList >
-        {tasks.map((task, index) => (
+        {searchTasks && searchTasks.map((task, index) => (
           <Task
               task={task}
               index={index}
@@ -78,9 +110,13 @@ function App() {
           />
         ))}
       </TodoList>
-      <Icon className='task-button' color='purple' name='add circle' size='huge' onClick={openFromParent}/>
-      <AddTask newTask={newTask} title={title} description={description}
-                IsModalOpened={modalIsOpen} onCloseModal={handleCloseModal} />
+      <Popup content='Add a new task' trigger={
+        <Icon className='task-button' color='green' name='add' size='huge' onClick={openFromParent}/>
+      } />
+      <Popup content='Delete all tasks' trigger={
+        <Icon className='delete-button' color='red' name='delete' size='huge' onClick={deleteAll}/>
+      } />
+      <AddTask newTask={newTask} title={title} description={description} IsModalOpened={modalIsOpen} onCloseModal={handleCloseModal} />
     </div>
   )
 }
